81c6d51aced2b6cbeb0febc1bab80a7ff27240f2
vishal <vishal.pvv@gmail.com>
2017-04-12 20:06:10 +0530

    v1.0

---
+++ src/com/vishal/findelement/test/Test.java
@@ -0,0 +1,13 @@
+package com.vishal.findelement.test;
+
+import com.vishal.findelement.FindElement;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println(FindElement.nonrepeatingElements("bangalore"));
+		System.out.println(FindElement.nonrepeatingElementsAt("india",3));
+
+	}
+
+}
